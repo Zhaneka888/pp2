7115cd29495ae272a40df5e97c2631b2dcae22d7
Zhaneka888 <zhanerkekaripbek@gmail.com>
2026-02-07 03:27:19 +0500

    Create practice2 folder and files

---
+++ Practice2/loops/while_continue.py
@@ -0,0 +1,37 @@
+#1
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+#2
+i = 0
+while i < 5:
+    i += 1
+    if i == 2:
+        continue
+    print(i)
+
+#3
+i = 0
+while i < 3:
+    i += 1
+    continue
+    print(i)
+
+#4
+i = 1
+while i <= 5:
+    if i == 4:
+        i += 1
+        continue
+    print(i)
+    i += 1
+
+#5
+i = 0
+while i < 1:
+    i += 1
+    continue
---
+++ Practice2/loops/while_loop.py
@@ -0,0 +1,29 @@
+#1
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+#2
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+#3
+i = 1
+while i <= 3:
+    print(i)
+    i += 1
+
+#4
+i = 1
+while i < 4:
+    print("Hello")
+    i += 1
+
+#5
+i = 1
+while i < 2:
+    print(i)
+    i += 1
