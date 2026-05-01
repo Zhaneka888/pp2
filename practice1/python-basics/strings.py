207449a9f7056a64b693e8d9e908f69f4d0c96bb
Zhaneka888 <zhanerkekaripbek@gmail.com>
2026-05-01 19:18:38 +0500

    practice1

---
+++ practice1/python-basics/strings.py
@@ -0,0 +1,20 @@
+#1 
+a = "Hello, World!"
+print(a[1])
+
+#2 
+a = "Hello, World!"
+print(len(a))
+
+#3
+b = "Hello, World!"
+print(b[2:5])
+
+#4
+a = "Hello, World!"
+print(a.upper())
+
+#5
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
---
+++ practice1/python-basics/variables.py
@@ -0,0 +1,31 @@
+#1
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#2
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#3
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#4
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#5
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
